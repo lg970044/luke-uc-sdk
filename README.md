350b3e38c5e758aa77fb357db10c91aad34ec4a4
liuxiong <liuxiong@luke.com>
2020-04-28 09:53:42 +0800

    类名规范化

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# luke-uc-sdk
+# luke/uc-sdk
 禄可统一认证中心
 
 ## 安装
 
-`composer require lg970044/luke-uc-sdk -vvv`
+`composer require luke/uc-sdk -vvv`
 
 ## 使用
 
 ```php
-$uc = new \Luke\Uc\Uc([
+$uc = new \Luke\Uc\Application([
     'base_url' => 'http://127.0.0.1:8001',
     'app_id' => 'test',
     'secret' => 'test'
--- src/Application.php
+++ src/Application.php
@@ -3,14 +3,14 @@ namespace Luke\Uc;
 use Hanson\Foundation\Foundation;
 
 /**
- * Class Uc
+ * Class Application
  * @package Luke\Uc
  *
  * @property \Luke\Uc\Sms       $sms
  * @property \Luke\Uc\User      $user
  * @property \Luke\Uc\Oauth     $oauth
  */
-class Uc extends Foundation
+class Application extends Foundation
 {
     protected $providers = [
         ServiceProvider::class
